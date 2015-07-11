6b1121c423cc0e49fead1411f0c8da1e4e04dd88
Angelo Ashmore <angeloashmore@me.com>
2015-07-10 18:23:30 -1000

    Removing create restriction for users

--- api/v1/resources/users.rb
+++ api/v1/resources/users.rb
@@ -14,7 +14,6 @@ module Doorbell
           post do
             validate_token!(client_id: ENV['DOORBELL_CLIENT_ID'],
                             client_secret: ENV['DOORBELL_CLIENT_SECRET'])
-            enforce_create_permission(User.new)
             args = { remote_id: @token['user_id'], email: @token['email'] }
             user = Mutation::Users::Create.run!(args)
             present user, with: Presenters::UserPresenter
